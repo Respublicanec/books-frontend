a7a70a776825cda68f8666d5137a4cab42d051b4
Yurii Kotov <respublicanec37@mail.ru>
2025-01-10 19:56:48 +0300

    Layout from scratch

---
+++ pages/auth.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <HeaderComponent />
+    <div class="login-container">
+      <h1>Вход</h1>
+      <input placeholder="Логин" class="login-input" type="text" />
+      <input placeholder="Пароль" class="login-input" type="text" />
+      <BaseButton class="base-button" backgroundColor="#696969" title="Войти" />
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.login-container {
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  width: 200px;
+  margin: auto;
+  margin-top: 200px;
+}
+.login-input {
+  margin: 20px 0 0 0;
+  height: 30px;
+  width: 100%;
+  border-radius: 3px;
+}
+.base-button {
+  height: 40px;
+  color: white;
+  border-radius: 40px;
+  width: 100px;
+  margin: 40px 0 0 60px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,18 @@
 <template>
-  <div>Hello!</div>
+  <div>
+    <HeaderComponent />
+  </div>
 </template>
 
-<script>
-export default {
-  name: "IndexPage",
-};
-</script>
+<script setup></script>
+
+<style>
+body {
+  border: 1px solid rgb(41, 181, 223);
+  height: 1080px;
+  width: 1920px;
+  margin: 0;
+  padding: 0;
+  font-family: "Roboto", sans-serif;
+}
+</style>
