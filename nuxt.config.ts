be307bb591104c7bb68f0689d54621809cd06059
mrPelkin <mrpelkin@gmail.com>
2025-01-14 09:08:16 +0300

    Add eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-04-03",
-  devtools: { enabled: true },
+
+  modules: ['@nuxt/eslint'],
+
+  devtools: { enabled: false },
+
   css: [
-    "@/assets/styles/main.css", // Путь к вашему файлу main.css
+    '@/assets/styles/main.css', // Путь к вашему файлу main.css
   ],
-});
+  compatibilityDate: '2024-04-03',
+
+  eslint: {
+    config: {
+      stylistic: {
+        arrowParens: true,
+      },
+    },
+  },
+})
